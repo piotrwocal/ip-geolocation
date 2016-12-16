f8c336dc5e4c643e8949be487de94dc028e992b3
Piotr Wocal <piotr.wocal@gmail.com>
2016-12-16 20:20:57 +0100

    move classes to new packages

--- src/ip_geolocation/quova_ip_format.clj
+++ src/ip_geolocation/quova_ip_format.clj
@@ -4,9 +4,11 @@
 
 (def octet-ratios [16777216 65536 256 1])
 
+
 (defn octetIp->longIp [octetIp]
 	(let [octet-parts (map #(Integer/valueOf %) (str/split octetIp #"\."))]
 		(apply + (map * octet-parts octet-ratios))))
+
 
 (defn longIp->octetIp [longIp]
 	(->> (map #(mod (quot longIp %) 256) octet-ratios)
--- test/ip_geolocation/quova_ip_format_test.clj
+++ test/ip_geolocation/quova_ip_format_test.clj
@@ -2,12 +2,14 @@
 	(:use [clojure.test :only [deftest is are]])
 	(:require [ip-geolocation.quova-ip-format :refer :all]))
 
+
 (deftest octetIp->longIp-test
 	(are [octetIp longIp] (= (octetIp->longIp octetIp) longIp)
 		"202.186.13.4" 3401190660
 		"0.0.0.37"		 37
 		"1"						 16777216)
 	(is (thrown? NumberFormatException (octetIp->longIp ""))))
+
 
 (deftest longIp->octetIp-test
   (are [longIp octetIp] (= (longIp->octetIp longIp) octetIp)
